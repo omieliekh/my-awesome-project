3fbe367523b44ac52d5ad5e7328281cf39c5fa9b
Oleksandr Mieliekh <meleh.alexandr@gmail.com>
2026-01-26 00:41:42 +0800

    Add data cleanup after Login unmount

--- Login.jsx
+++ Login.jsx
@@ -3,6 +3,10 @@ const API = 'http://localhost:3000/login';
 export default function Login() {
   useEffect(() => {
     fetch(API).then(res => res.json()).then(data => console.log(data));
+
+    return () => {
+      console.log('Data cleanup')
+    }
   }, []);
 
   return (
