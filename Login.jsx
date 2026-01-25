718e770e774a9febdc7ef041290984de31b169c4
Oleksandr Mieliekh <meleh.alexandr@gmail.com>
2026-01-26 00:36:58 +0800

    Add proper Login page structure

--- Login.jsx
+++ Login.jsx
@@ -1,1 +1,10 @@
-export default function Login() { return <div>Login Page</div> }
+export default function Login() {
+  return (
+    <>
+      <div>Login Page</div>
+      <input type="text" name="email" placeholder="Email" />
+      <input type="password" name="password" placeholder="Password" />
+      <button>Login</button>
+    </>
+  )
+}
